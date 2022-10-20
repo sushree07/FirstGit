332adb9f86643c97be382ff50e5ce74a959990d9
sushree07 <sushreehoney07@gmail.com>
2022-10-20 13:30:24 +0530

    domTextBoldAndGreen

---
+++ dom.js
@@ -0,0 +1,7 @@
+var headerTitle = document.getElementById('header-title');
+var header =document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000';
+var title = document.getElementsByClassName('title');
+//console.log(title[0]);
+title[0].style.fontWeight='bold';
+title[0].style.color='green';  
