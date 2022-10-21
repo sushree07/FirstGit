412959393d52941d1642b35e4ed114daebdb9e54
sushree07 <sushreehoney07@gmail.com>
2022-10-21 10:44:31 +0530

    quarySelector

--- dom.js
+++ dom.js
@@ -1,13 +1,6 @@
-
-// getelementbytagname
- var li = document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].textContent='Hello2';
-li[1].style.fontWeight='bold';
-li[1].style.backgroundColor='yellow';  
-
-for(var i=0; i < li.length;i++){
-    li[i].style.backgroundColor='gray';
-}
-//tag applies to tag but class applies to only class not tag
+var item =document.querySelector('.list-group-item');
+item.style.color='red';
+var seconditem =document.querySelector('.list-group-item:nth-child(2)');
+seconditem.style.backgroundColor="green"; 
+ var thirditem =document.querySelector('.list-group-item:nth-child(3)');
+thirditem.style.color="white"; 
