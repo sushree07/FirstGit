e9ba1b1972256678cc20a83d6820bce63545869d
sushree07 <sushreehoney07@gmail.com>
2022-11-04 01:38:55 +0530

    task_8_9_filter

--- dom.js
+++ dom.js
@@ -15,13 +15,20 @@ function addItem(e){
 
   // Get input value
   var newItem = document.getElementById('item').value;
+  var description = document.getElementById('description').value;
+
 
   // Create new li element
   var li = document.createElement('li');
   // Add class
   li.className = 'list-group-item';
   // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
+  //li.appendChild(document.createTextNode(newItem));
+
+  const newText = document.createTextNode(newItem)
+  const descriptionNode= document.createTextNode(" "+description)
+  li.appendChild(newText);
+  li.appendChild(descriptionNode);
 
   // Create del button element
   var deleteBtn = document.createElement('button');
@@ -49,3 +56,20 @@ function removeItem(e){
   }
 }
 
+  // Filter Items
+function filterItems(e){
+  // convert text to lowercase
+  const text = e.target.value.toLowerCase();
+  // Get lis
+  const items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    const itemName = item.firstChild.textContent;
+    const description= item.childNodes[1].textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1 || description.toLowerCase().indexOf(text) !=-1 ){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  }
+  )}
